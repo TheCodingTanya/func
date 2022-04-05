a84092fa46a8c700df96890f3057b4b8240015c5
Tanya <thetwtanya@gmail.com>
2022-04-05 19:45:58 +0800

    with water level 8

--- func.py
+++ func.py
@@ -2,16 +2,17 @@
 
 # function是用來[收納]程式碼的
 # 它是個功能不執行
+dry = True
 
-def wash(dry): # def是define, dry是參數
-	print('加水')
-	print('加洗衣精')
-	print('旋轉')
-	if dry:
-		print('烘衣')
+def wash(dry=False, water=8): # def是define, dry是參數
+    print('加水', water, '分滿')
+    print('加洗衣精')
+    print('旋轉')
+    if dry:
+        print('烘衣')
 
-wash(True) # 使用functionR, True投進dry, 印出烘衣
-wash(False) #第二次將False投進, 就沒烘衣
+wash()
+# wash() # 使用function
 
 def say_hi():
 	print('hi')
